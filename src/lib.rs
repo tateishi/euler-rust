224b0105145c9aa77d40dc932cb425d8d103666c
Tateishi Tadatoshi <ishio39@gmail.com>
2021-12-12 10:59:48 +0900

    テストコードをモジュールに移動

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,33 @@ pub mod pr001 {
     pub fn answer(n: i32) -> i32 {
         (1..n).filter(|&x| mod_3_or_5(x)).sum()
     }
-}
 
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test_mod_3_or_5() {
+            use super::mod_3_or_5;
+
+            assert_eq!(mod_3_or_5(1), false);
+            assert_eq!(mod_3_or_5(2), false);
+            assert_eq!(mod_3_or_5(3), true);
+            assert_eq!(mod_3_or_5(4), false);
+            assert_eq!(mod_3_or_5(5), true);
+            assert_eq!(mod_3_or_5(6), true);
+            assert_eq!(mod_3_or_5(7), false);
+            assert_eq!(mod_3_or_5(8), false);
+            assert_eq!(mod_3_or_5(9), true);
+            assert_eq!(mod_3_or_5(10), true);
+        }
+
+        #[test]
+        fn test() {
+            use super::answer;
+
+            assert_eq!(answer(10), 23);
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -21,26 +46,4 @@ mod tests {
         assert_eq!(result, 4);
     }
 
-    #[test]
-    fn test_pr001_mod_3_or_5() {
-        use super::pr001::mod_3_or_5;
-
-        assert_eq!(mod_3_or_5(1), false);
-        assert_eq!(mod_3_or_5(2), false);
-        assert_eq!(mod_3_or_5(3), true);
-        assert_eq!(mod_3_or_5(4), false);
-        assert_eq!(mod_3_or_5(5), true);
-        assert_eq!(mod_3_or_5(6), true);
-        assert_eq!(mod_3_or_5(7), false);
-        assert_eq!(mod_3_or_5(8), false);
-        assert_eq!(mod_3_or_5(9), true);
-        assert_eq!(mod_3_or_5(10), true);
-    }
-
-    #[test]
-    fn test_pr001() {
-        use super::pr001::answer;
-
-        assert_eq!(answer(10), 23);
-    }
 }
