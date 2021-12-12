20a9736a000ca44f0fe2cd0463d85548087292ad
Tateishi Tadatoshi <ishio39@gmail.com>
2021-12-12 19:59:54 +0900

    problem 2

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
-    use euler::pr001::answer;
+    use euler::pr001;
+    use euler::pr002;
 
-    println!("The answer is {}", answer(1000));
+    println!("Problem 001, the answer is {}", pr001::answer(1000));
+    println!("Problem 002, the answer is {}", pr002::answer(40000000));
 }
