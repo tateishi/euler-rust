f89322dc3ad8060015f165dee0a032240f45e51a
Tateishi Tadatoshi <ishio39@gmail.com>
2021-12-21 23:44:51 +0900

    prime.rs の使用

--- src/pr003.rs
+++ src/pr003.rs
@@ -1,37 +1,4 @@
-
-const WHEEL30: [u64; 8] = [7, 11, 13, 17, 19, 23, 29, 31];
-
-pub struct Wheel30 {
-    index: usize,
-    base: u64,
-}
-
-impl Wheel30 {
-    pub fn new() -> Wheel30 {
-        Wheel30 {
-            index: 0, base: 0,
-        }
-    }
-}
-
-impl Iterator for Wheel30 {
-    type Item = u64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let value = self.base + WHEEL30[self.index];
-        self.index += 1;
-        if self.index > WHEEL30.len() - 1 {
-            self.index = 0;
-            self.base += 30;
-        }
-        Some(value)
-    }
-}
-
-
-pub fn maybe_primenumber() -> impl Iterator<Item=u64> {
-    [2, 3, 5].into_iter().chain(Wheel30::new())
-}
+use crate::prime::maybe_primenumber;
 
 
 pub fn factors(n: u64) -> Vec<u64> {
